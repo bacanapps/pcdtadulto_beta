bfbd2d05fd5de59dc84caf39225201f768c7de9c
bacanapps <apps@bacanapps.com>
2014-02-13 09:04:34 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+pcdt adulto
+====
+
+Protocolo Clinico e Diretrizes Terapêuticas – PCDT adulto
+http://www.aids.gov.br/pcdt
+
+Colin Pantin
+colin.pantin@aids.gov.br
+
+
---
+++ scripts/imgac.js
@@ -0,0 +1,44 @@
+document.addEventListener("deviceready", init, false);
+var AnalyticsAccount = "UA-47180490-3";
+
+
+function errorHandler(e) {
+		alert(e.toString());
+}
+
+
+function init() {
+		gaPlugin = window.plugins.gaPlugin;
+		gaPlugin.init(gaInit, errorHandler, AnalyticsAccount, 10);	
+		
+		
+		$(".linkclick").click(function(e){
+			e.preventDefault();
+			var textdata = $(this).html();
+			var intdata = parseInt($(".linkclick").index(this));
+			
+			
+			gaPlugin.trackEvent(function() {
+				
+				
+			}, errorHandler, "Button", "Click", textdata, intdata);	
+
+			
+
+		});
+	
+}
+
+function gaInit() {
+		
+}
+
+
+
+
+
+
+
+
+
+
